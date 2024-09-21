291721ca50d37dcf83613d3e0e401bb3a2673dda
brent-b-c <brentcodiamat642@gmail.com>
2024-09-21 22:47:53 +0800

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+  public static void main(String[] args) {
+    String name = "Brent Codiamat";
+    int age = 18;
+    float GPA = 1.5f;
+    boolean MyBool = false;
+    int a = 20 , b = 60;
+
+    
+  System.out.println("Hello, my name is " + name +"and I am " + age + " years old.");
+  System.out.println("I have a GPA of " + GPA + " and my grade is A");
+  System.out.println("Graduated ? " + MyBool);
+  System.out.println("sum: " + (a + b));
+  System.out.println("difference: " + (a - b));
+  System.out.println("product: " + (a * b));
+  System.out.println("quotient: " + (a / b));
+  
+  
+  }
+
+
+
+
+}
