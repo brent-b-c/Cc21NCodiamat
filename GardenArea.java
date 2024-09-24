a0a2e22c1147af5cfd9a037330e7a6deae052d57
brent-b-c <brentcodiamat642@gmail.com>
2024-09-24 15:15:46 +0800

    Update GardenArea.java

--- GardenArea.java
+++ GardenArea.java
@@ -1,18 +1,19 @@
 //Brent B. Codiamat
+//Second Laboratory Exercise
 //CITCS CC2 1N-A
 
 public class GardenArea {
     public static void main(String[] args ) {
-        int length = 50; 
-        int width = 30;
-        float area = length * width;
-        float perimeter = 2 * (length + width);
+        int length = 50; //length of the garden
+        int width = 30;  //width of the garden
+        double area = length * width, perimeter = 2;//formula for getting the area 
+        
 
-        System.out.println("The length of the garden is " + width + " meters");
-        System.out.println("The width of the garden is " + length + " meters");
-        System.out.println("\n\n");
-        System.out.println("The area of the garden is " + area + " square meters");
-        System.out.println("The perimeter of the garden is " + perimeter + " square meters");
+        System.out.println("The length of the garden is " + width + " meters"); //this will print the width of the garden
+        System.out.println("The width of the garden is " + length + " meters"); //this wil print the width of the garden
+        System.out.println("\n\n"); //for new line
+        System.out.println("The area of the garden is " + area + " square meters"); //this will print the area of the garden
+        System.out.println("The perimeter of the garden is " + perimeter + " square meters"); //this will print the perimeter of the garden
 
     }
 }
