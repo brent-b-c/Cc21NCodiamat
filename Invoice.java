4559ee27aab16f4038acbfcbb26405fe32676446
brent-b-c <brentcodiamat642@gmail.com>
2024-09-24 15:15:27 +0800

    Update Invoice.java

--- Invoice.java
+++ Invoice.java
@@ -1,26 +1,25 @@
-
 //FIRST LABORATORY EXERCISE
 //BRENT B. CODIAMAT
 //SEPTEMBER 3, 2024
 public class Invoice {
     public static void main (String[] args) {
-      System.out.println("\t\t\t\t\t\t\"Brent's Refreshers\"");
-      System.out.println("\t\t\t\t\t\t   Agoo, La Union");
+      System.out.println("\t\t\t\t\t\t\"Brent's Refreshers\""); // name of the shop
+      System.out.println("\t\t\t\t\t\t   Agoo, La Union"); // address of the shop
       System.out.println("\n");
-      System.out.print("Billing to: Brent");
-      System.out.println("\t\t\t\t\t\t\t    September 3, 2024");
-      System.out.println("\t\t\t\t\t\t\t\t\t\t\t    Invoice No. 343434");
-      System.out.println("======================================================================");
+      System.out.print("Billing to: Brent");//name of the customer
+      System.out.println("\t\t\t\t\t\t\t\t             September 3, 2024");//date
+      System.out.println("\t\t\t\t\t\t\t\t\t\t\t    Invoice No. 343434");//invoice number
+      System.out.println("==============================================================================================================");
       System.out.print("Item/s:");
-      System.out.print("\t\t\t\t\tQTY:");
+      System.out.print("\t\t\t\t\t\t\tQTY:");//quantity of the item
       System.out.print("\t\t\tPrice:");
-      System.out.print("\t\t\t  Amount:");
+      System.out.print("\t\t      Amount:");//amount of item
       System.out.println("\n");
-      System.out.print("Blueberry");
+      System.out.print("Blueberry");      //product
       System.out.print("\t\t\t\t\t1");
-      System.out.print("\t\t\t\t25.00");
-      System.out.println("\t\t\t25.00");
-      System.out.print("Strawberry");
+      System.out.print("\t\t\t\t25.00");  //price
+      System.out.println("\t\t\t25.00");  //price
+      System.out.print("Strawberry");     //name of product
       System.out.print("\t\t\t\t\t2");
       System.out.print("\t\t\t\t25.00");
       System.out.println("\t\t\t50.00");
@@ -28,10 +27,10 @@ public class Invoice {
       System.out.print("\t\t\t\t\t\t1");
       System.out.print("\t\t\t\t25.00");
       System.out.println("\t\t\t25.00");
-      System.out.println("-----------------------------------------------------------------");
+      System.out.println("--------------------------------------------------------------------------------------------------------------");
       System.out.print("TOTAL:");
-      System.out.println("\t\t\t\t\t\t\t\t\t\t       100.00");
-   System.out.println("=================================================================");
+      System.out.println("\t\t\t\t\t\t\t\t\t\t\t             PHP100.00");//final amount
+      System.out.println("==============================================================================================================");
       System.out.print("\t\t\t\t\tTHANK YOU FOR SHOPPING WITH US!");
 
     }
